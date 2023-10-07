bec31f180bdb4292143eb916d966c9c73346d42c
Alysha Pursley <117888047+aspradlin2019@users.noreply.github.com>
2023-10-07 03:33:37 -0400

    Update main.py
    Added more code comments, fixed some errors in code. If you have any suggestions or requests, feel free to contact me on facebook @Alysha Pursley or LinkedIn or wherever at same name! Working on making it actually add the images to the spreadsheet but I cant get the code correct for that for some reason and it breaks everything but as soon as I do I'll update this again! Happy coding!

--- main.py
+++ main.py
@@ -16,24 +16,28 @@ from openpyxl.drawing.image import Image
 # Function to check for CAPTCHA and pause if detected
 print("Script starting...")
 print("Checking for CAPTCHA...")
-
+time.sleep(15)
 def check_for_captcha():
+    time.sleep(15)
     captcha_elements = driver.find_elements(By.XPATH, "//input[contains(@id, 'capt')]")
     if captcha_elements:
         input("CAPTCHA detected! Please solve the CAPTCHA and press Enter to continue...")
+
 print("Checking for 2FA...")
 
 def handle_2fa():
     # Wait for a short duration to see if the 2FA page loads
-    time.sleep(3)
+    time.sleep(15) #you may need to change these throughout the code to a larger number if you get an error. This makes the wait longer and will ensure that the elements have loaded. Clear Cache and cookies if you still get errors and run again.
     # Check if the 2FA element is present
     two_fa_elements = driver.find_elements(By.ID, "twoFactorAuth")
     if two_fa_elements:
         print("2FA detected!")
         input("Please complete the 2FA step and then press Enter to continue...")
+        # Sleep for a bit after manual intervention
+        time.sleep(15)
 
 # Set ChromeDriver path
-chrome_driver_path = r"YOUR_CHROMEDRIVER_PATH_HERE"
+chrome_driver_path = r"YOUR_CHROMEDRIVER_EXECUTABLE_PATH_HERE"
 
 # Setting up Selenium
 chrome_options = Options()
@@ -41,15 +45,17 @@ service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Image save directory
-IMAGE_SAVE_DIRECTORY = r"YOUR_IMAGE_OUTPUT_PATH/SAVE_DIRECTORY_PATH_HERE"
+IMAGE_SAVE_DIRECTORY = r"YOUR_IMAGE_SAVE_PATH_HERE" #If you get any errors stating the path doesn't exist, change "\" to either "/" or "\\". This goes for all path code
 if not os.path.exists(IMAGE_SAVE_DIRECTORY):
     os.makedirs(IMAGE_SAVE_DIRECTORY)
 
-# Spreadsheet paths
-ORIGINAL_SPREADSHEET_PATH = r"YOUT_INPUT_SPREADSHEET_PATH_HERE"
+# Input spreadsheet paths
+ORIGINAL_SPREADSHEET_PATH = r"YOUR_SPREADSHEET_PATH_HERE"
 ORIGINAL_SPREADSHEET_NAME = os.path.basename(ORIGINAL_SPREADSHEET_PATH).replace('.xlsx', '')
 
+# Updated spreadsheet output path
 CHECKPOINT_FILE = 'last_processed_product.txt'
+save_directory = r"YOUR_UPDATED_SHEET_SAVE_PATH_HERE"
 
 # Load the last checkpoint if it exists
 last_processed_product = None
@@ -64,45 +70,48 @@ check_for_captcha()
 
 # 2. Hover over the Sign-In dropdown
 print("Hovering over the Sign-In dropdown...")
+time.sleep(10)
 hover_element = driver.find_element(By.ID, "nav-link-accountList-nav-line-1")
 ActionChains(driver).move_to_element(hover_element).perform()
-time.sleep(5)
+time.sleep(10)
 check_for_captcha()
 
 # 3. Click on Sign In
 print("Clicking on Sign In...")
+time.sleep(10)
 driver.find_element(By.XPATH, "//span[text()='Sign in']").click()
 check_for_captcha()
 
 # 4. Enter Email & Continue
 print("Entering email and proceeding...")
-driver.find_element(By.ID, "ap_email").send_keys("YOUR_EMAIL/YOUR_USERNAME_HERE")
+time.sleep(10)
+driver.find_element(By.ID, "ap_email").send_keys("YOUR_AMAZON_USERNAME_HERE")
+time.sleep(10)
 driver.find_element(By.ID, "continue").click()
-time.sleep(5)
 check_for_captcha()
 
 # 5. Enter Password & Sign In
 print("Entering password and signing in...")
-driver.find_element(By.ID, "ap_password").send_keys("YOUR_LOGIN_PASSWORD_HERE")
-time.sleep(5)
+time.sleep(10)
+driver.find_element(By.ID, "ap_password").send_keys("YOUR_AMAZON_PASSWORD_HERE")
+time.sleep(10)
 check_for_captcha()
 
+#This code segment is supposed to stop to allow you to manually solve captcha or enter code for 2FA, but it doesn't. Working on it for next update. If the code moves on to the next task before you have the chance
+#/nto enter the code or solve the captcha make the time.sleep function longer. I'll add comment there. Always enter the time you want it to wait in seconds.
 # 6. Handle manual CAPTCHA & 2FA
 try:
     signInButton = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.ID, "signInSubmit")))
+    time.sleep(60) #this will need to be increased if you need more time to solve captcha or enter code for 2FA. Currently set at 60 seconds.
     signInButton.click()
     print("Sign In button clicked.")
 except:
     print("Failed to click the Sign In button.")
 
-save_directory = r"YOUR_OUTPUT_PATH/SAVE_DIRECTORY_HERE"
-if not os.path.exists(save_directory):
-    os.makedirs(save_directory)
-
 # Load the spreadsheet
 print("Loading the spreadsheet...")
-original_file_path = r"YOUR_INPUT_SPREADSHEET_PATH_HERE"
+original_file_path = r"YOUR_SPREADSHEET_PATH_HERE"
 original_file_name = os.path.basename(original_file_path)
 df = pd.read_excel(original_file_path)
 
@@ -118,7 +127,7 @@ df['Product Images'] = df['Product Images'].astype(str)
 df['Product Description'] = df['Product Description'].astype(str)
 
 # Create a directory to store images if it doesn't exist
-save_directory = "Product Images"
+IMAGE_SAVE_DIRECTORY = "YOUR_IMAGE_SAVE_PATH_HERE"
 if not os.path.exists(save_directory):
     os.makedirs(save_directory)
 
@@ -152,7 +161,7 @@ for index, row in df.iterrows():
         except Exception as e:
             print(f"Attempt {retry_count + 1}: Failed to navigate to 'Your Orders'. Trying again...")
             retry_count += 1
-            time.sleep(5)  # Wait for 5 seconds before retrying
+            time.sleep(15)  # Wait for 10 seconds before retrying
 
     # Click on 'Your Orders' link
     your_orders_link = WebDriverWait(driver, 10).until(
@@ -160,25 +169,47 @@ for index, row in df.iterrows():
     )
     your_orders_link.click()
 
-    # Search for the product using the search bar
+    # Click the arrow on the dropdown to open it
+    try:
+        dropdown_arrow = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, "span#a-autoid-4-announce.a-button-text.a-declarative"))
+        )
+        dropdown_arrow.click()
+    except Exception as e:
+        print(f"Failed to click the dropdown arrow. Error: {e}")
+
+    # Click the "2023" element to filter by year
+    try:
+        year_2023_element = driver.find_element(By.ID, "orderFilter_2")
+        year_2023_element.click()
+        print("Clicked on '2023' to filter orders.")
+    except Exception as e:
+        print(f"Failed to click '2023'. Error: {e}")
+
+   # Search for the product using the search bar
     try:
         search_input = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, "searchOrdersInput"))
         )
+        time.sleep(5)
         search_input.clear()
+        time.sleep(5)
         search_input.send_keys(product_name)
+        time.sleep(5)
         search_button = driver.find_element(
             By.CSS_SELECTOR, "input.a-button-input[aria-labelledby='a-autoid-3-announce']"
         )
+        time.sleep(5)
         search_button.click()
     except Exception as e:
         print(f"Failed to search for product. Error: {e}")
-
+    
     # Click on the product link
     try:
         product_link = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, f"//a[contains(text(), '{product_name}')]"))
         )
+        time.sleep(5)
         product_link.click()
     except Exception as e:
         print(f"Couldn't find product listing for: {product_name}. Error: {e}")
@@ -199,7 +230,7 @@ for index, row in df.iterrows():
                 if img_url:
                     response = requests.get(img_url, stream=True)
                     file_name = f"{product_name}_image_{idx}.jpg".replace(" ", "_")
-                    file_path = os.path.join(save_directory, file_name)
+                    file_path = os.path.join(IMAGE_SAVE_DIRECTORY, file_name)
                     with open(file_path, 'wb') as out_file:
                         out_file.write(response.content)
                     saved_image_paths.append(file_path)
@@ -277,15 +308,15 @@ for index, row in df.iterrows():
 
     # Navigate back to "Your Orders"
     try:
-        # Hover over "Hello, Alysha"
-        hover_element = driver.find_element(By.XPATH, "//span[text()='Hello, YOUR_ACCOUNT_NAME_HERE']")
+        # Hover over "Hello, 'YOUR NAME ON AMAZON'"
+        hover_element = driver.find_element(By.XPATH, "//span[text()='Hello, 'YOUR NAME ON AMAZON'']") #CHANGE 'YOUR NAME ON AMAZON' TO THE NAME THAT SHOWS ON THE TOP NAV BAR ON HOME SCREEN
         ActionChains(driver).move_to_element(hover_element).perform()
-        time.sleep(2)
+        time.sleep(5)
 
         # Click on "Your Orders"
         your_orders_element = driver.find_element(By.XPATH, "//span[text()='Your Orders']")
         your_orders_element.click()
-        time.sleep(2)
+        time.sleep(5)
     except Exception as e:
         print(f"Failed to navigate back to 'Your Orders'. Error: {e}")
 
@@ -299,4 +330,4 @@ df.to_excel(final_spreadsheet_path, index=False)
 # Don't forget to close the driver when done
 driver.quit()
 
-print("Script completed successfully!")
+print("Script completed successfully!") #YOU SHOULD NOW HAVE ALL OF YOUR ITEMS IMAGES AND THE LOCAL IMAGE SAVE PATHS IN THE SPREADSHEET, AS WELL AS ALL OF THE PRODUCT DESCRIPTIONS (AS LONG AS THEY WERE FOUND)
